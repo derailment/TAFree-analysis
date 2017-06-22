388e5697a9b485e21621f6e320969f82e24a0055
root <root@TAFree>
2017-06-22 20:18:57 +0800

    Make genRel.R more general

--- README.md
+++ README.md
@@ -25,5 +25,5 @@ sudo php Mover.php ./log/Lab01/Lab01_1.csv
 sudo php Accumulator.php Lab01 1
 sudo php Fetcher.php Lab01 Lab02 Lab03 Lab04
 sudo Rscript predictor.R ./train/DataQuiz1.csv ./pred/PredQuiz1.csv
-sudo Rscript genRel.R ./train/DataQuiz1.csv
+sudo Rscript genRel.R ./train/DataQuiz1.csv WA Submissions 0.86 0.01
 ```
--- genRel.R
+++ genRel.R
@@ -7,57 +7,95 @@
 # License:      MIT
 
 
-# Read input file and output file
+# Read input file(.csv)
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
-	stop("Please supply input file(.csv) and output file(.csv)")
+	stop("Please supply input file(.csv) and (x, y) and (r, p) parameters.")
+} else if (length(args)==1){
+	stop("Please supply (x, y) and (r, p) parameters.")
+} else if (length(args)==2){
+	stop("Please supply y and (r, p) parameters.")	
+} else if (length(args)==3){
+	stop("Please supply (r, p) parameters.")	
+} else if (length(args)==4){
+	stop("Please supply p parameter.")	
 }
 input = read.csv(file=args[1], sep=",", header=TRUE)
 summary(input)
 
 
-# Generate images
-# Take Avg. Sub. as y label
-y <- input[['Submissions']]
-x <- input[['WA']]
-jpeg('SUB_WA.jpg')
-plot(x, y, xlim=c(0, max(x)+5), ylim=c(0, max(y)+5), main='Avg. Sub. vs. Avg. WA', xlab='Avg. WA', ylab='Avg. Sub.')
-abline(lsfit(x, y))
-dev.off()
-x <- input[['CE']]
-jpeg('SUB_CE.jpg')
-plot(x, y, xlim=c(0, max(x)+5), ylim=c(0, max(y)+5), main='Avg. Sub. vs. Avg. CE', xlab='Avg. CE', ylab='Avg. Sub.')
-abline(lsfit(x, y))
-dev.off()
-x <- input[['RE']]
-jpeg('SUB_RE.jpg')
-plot(x, y, xlim=c(0, max(x)+5), ylim=c(0, max(y)+5), main='Avg. Sub. vs. Avg. RE', xlab='Avg. RE', ylab='Avg. Sub.')
-abline(lsfit(x, y))
-dev.off()
-x <- input[['NA.']]
-jpeg('SUB_NA.jpg')
-plot(x, y, xlim=c(0, max(x)+5), ylim=c(0, max(y)+5), main='Avg. Sub. vs. Avg. NA', xlab='Avg. NA', ylab='Avg. Sub.')
-abline(lsfit(x, y))
-dev.off()
+# Read (r, p)
+r <- args[4]
+p <- args[5]
 
-# Take Finish Rate as y label
-y <- input[['Finish_Rate...']]
-x <- input[['CE']]
-jpeg('FIN_CE.jpg')
-plot(x, y, xlim=c(0, max(x)+5), ylim=c(0, 100), main='Finish Rate vs. Avg. CE', xlab='Avg. CE', ylab='Finish Rate (%)')
-abline(lsfit(x, y))
-dev.off()
 
-# Take Score as y label
-y <- input[['Score']]
-x <- input[['Finish_Rate...']]
-jpeg('SCO_FIN.jpg')
-plot(x, y, xlim=c(0, max(x)+5), ylim=c(0, 100), main='Score vs. Finish Rate', xlab='Finish Rate (%)', ylab='Score')
-abline(lsfit(x, y))
-dev.off()
-x <- input[['TLE']]
-jpeg('SCO_TLE.jpg')
-plot(x, y, xlim=c(0, max(x)+5), ylim=c(0, 100), main='Score vs. Avg. TLE', xlab='Avg. TLE', ylab='Score')
-abline(lsfit(x, y))
-dev.off()
+# Read (x, y)
+xlab <- args[2]
+ylab <- args[3]
+x <- input[[xlab]]
+y <- input[[ylab]]
+xmax <- 0
+ymax <- 0
+if(xlab=="Submissions") {
+	xlab <- "Avg. Sub."
+	xmax <- max(x)+5 
+} else if(xlab=="WA"){
+	xlab <- "Avg. WA"
+	xmax <- max(x)+5
+} else if(xlab=="NA."){
+	xlab <- "Avg. NA"
+	xmax <- max(x)+5
+} else if(xlab=="CE"){
+	xlab <- "Avg. CE"
+	xmax <- max(x)+5
+} else if(xlab=="RE"){
+	xlab <- "Avg. RE"
+	xmax <- max(x)+5
+} else if(xlab=="TLE"){
+	xlab <-"Avg. TLE"
+	xmax <- max(x)+5
+} else if(xlab=="Score"){
+	xlab <- "Score"
+	xmax <- 100
+} else if(xlab=="Finish_Rate..."){
+	xlab <- "Finish Rate"
+	xmax <- 100
+} else if(xlab=="Average_Time.Min."){
+	xlab <- "Avg. Int. (Min)"
+	xmax <- max(x)+5
+}
+if(ylab=="Submissions") {
+	ylab <- "Avg. Sub."
+	ymax <- max(y)+5 
+} else if(ylab=="WA"){
+	ylab <- "Avg. WA"
+	ymax <- max(y)+5
+} else if(ylab=="NA."){
+	ylab <- "Avg. NA"
+	ymax <- max(y)+5
+} else if(ylab=="CE"){
+	ylab <- "Avg. CE"
+	ymax <- max(y)+5
+} else if(ylab=="RE"){
+	ylab <- "Avg. RE"
+	ymax <- max(y)+5
+} else if(ylab=="TLE"){
+	ylab <-"Avg. TLE"
+	ymax <- max(y)+5
+} else if(ylab=="Score"){
+	ylab <- "Score"
+	ymax <- 100
+} else if(ylab=="Finish_Rate..."){
+	ylab <- "Finish Rate"
+	ymax <- 100
+} else if(ylab=="Average_Time.Min."){
+	ylab <- "Avg. Int. (Min)"
+	ymax <- max(y)+5
+}
 
+
+# Generate an image
+jpeg(paste(xlab, '_', ylab,'.jpg'))
+plot(x, y, xlim=c(0, xmax), ylim=c(0, ymax), main=paste(xlab, ' vs. ', ylab), sub=paste("(r=", r,"; p<=", p, ")"), xlab=xlab, ylab=ylab)
+abline(lsfit(x, y))
+dev.off()
